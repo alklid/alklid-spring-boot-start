45f6cd75ed246e8f1d0fa15883a7c01e97100d16
alklid <alklid@gmail.com>
2019-01-12 22:50:07 +0900

    use @ConfigurationProperties

--- src/main/java/me/alklid/HolomanConfiguration.java
+++ src/main/java/me/alklid/HolomanConfiguration.java
@@ -1,18 +1,20 @@
 package me.alklid;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
 public class HolomanConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public Holoman holoman() {
+    public Holoman holoman(HolomanProperties prop) {
         Holoman holoman = new Holoman();
-        holoman.setHowLong(5);
-        holoman.setName("alklid");
+        holoman.setHowLong(prop.getHowLong());
+        holoman.setName(prop.getName()) ;
 
         return holoman;
     }
